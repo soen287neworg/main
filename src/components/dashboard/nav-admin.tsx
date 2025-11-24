8ff556710e1f7da3a0fdd52d4dad571e858aa4f4
Taras Baranovskyy <2000btv@gmail.com>
2025-11-24 18:52:10 -0500

    hmmm

--- src/components/dashboard/nav-admin.tsx
+++ src/components/dashboard/nav-admin.tsx
@@ -36,34 +36,41 @@ export function NavAdmin({
       <SidebarGroupLabel>Administration</SidebarGroupLabel>
       <SidebarMenu>
         {admin.map((item) => (
-          <Collapsible key={item.name} asChild className="group/collapsible">
-            <SidebarMenuItem>
-              <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip={item.name}>
+          <SidebarMenuItem key={item.name}>
+            {item.items ? (
+              <Collapsible asChild className="group/collapsible">
+                <>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={item.name}>
+                      <item.icon />
+                      <span>{item.name}</span>
+                      <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.items.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton asChild>
+                            <a href={subItem.url}>
+                              <span>{subItem.title}</span>
+                            </a>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </>
+              </Collapsible>
+            ) : (
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
                   <item.icon />
                   <span>{item.name}</span>
-                  {item.items && (
-                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                  )}
-                </SidebarMenuButton>
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-                {item.items && (
-                  <SidebarMenuSub>
-                    {item.items.map((subItem) => (
-                      <SidebarMenuSubItem key={subItem.title}>
-                        <SidebarMenuSubButton asChild>
-                          <a href={subItem.url}>
-                            <span>{subItem.title}</span>
-                          </a>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                )}
-              </CollapsibleContent>
-            </SidebarMenuItem>
-          </Collapsible>
+                </a>
+              </SidebarMenuButton>
+            )}
+          </SidebarMenuItem>
         ))}
       </SidebarMenu>
     </SidebarGroup>
